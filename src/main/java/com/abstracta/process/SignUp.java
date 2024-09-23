734614a70d92a61b9af90277f72c97a37f1038ff
Ramses Ortiz <ramseso@iadb.org>
2024-09-23 01:10:21 -0300

    Signup process completed

--- src/main/java/com/abstracta/process/SignUp.java
+++ src/main/java/com/abstracta/process/SignUp.java
@@ -1,4 +1,50 @@
 package com.abstracta.process;
 
-public class SignUp {
+import com.abstracta.config.ConfigPage;
+import com.abstracta.config.LoggerPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SignUp extends ConfigPage {
+
+    private static final By signUpLink = By.id("signin2");
+    private static final By usernameInput = By.id("sign-username");
+    private static final By passwordInput = By.id("sign-password");
+    private static final By signUpBtn = By.cssSelector("button[onclick='register()']");
+
+    public SignUp(WebDriver driver) {
+        super(driver);
+    }
+
+    public static void accessToSignUp() {
+        waitElement(signUpLink, 10);
+        click(signUpLink);
+        waitElement(usernameInput, 5);
+        if (isDisplayed(usernameInput)) {
+            LoggerPage.logEvent("SIGN_UP", "Sign Up successfully selected");
+            screenshot("Sign Up selected");
+        }
+    }
+
+    public static void fillData(String username, String password) {
+        waitElement(usernameInput, 5);
+        type(usernameInput, username);
+        type(passwordInput, password);
+        LoggerPage.logEvent("SIGN_UP", "Sign Up data successfully filled");
+        screenshot("Sign Up data filled");
+        click(signUpBtn);
+    }
+
+    public static boolean isSignUpConfirmationDisplayed() {
+        if (isAlertPresent()) {
+            LoggerPage.logEvent("SIGN_UP", "Sign Up process successfully confirmed");
+            screenshot("Sign Up process confirmed");
+            acceptAlert();
+            return true;
+        } else {
+            LoggerPage.logEvent("SIGN_UP", "Sign Up process could not be confirmed");
+            screenshot("Sign Up process error");
+            return false;
+        }
+    }
 }
--- src/test/java/com/abstracta/defs/SignUpTest.java
+++ src/test/java/com/abstracta/defs/SignUpTest.java
@@ -1,8 +1,11 @@
 package com.abstracta.defs;
 
+import com.abstracta.config.ConfigPage;
+import com.abstracta.process.SignUp;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 
 import static com.abstracta.utils.PropertyManager.getProperty;
 
@@ -10,17 +13,21 @@ public class SignUpTest {
 
     @Given("a user visits the website to sign up")
     public void a_user_visits_the_website_to_sign_up() {
+        ConfigPage.getUrl(getProperty("url"));
     }
 
     @When("access to sign up")
     public void access_to_sign_up() {
+        SignUp.accessToSignUp();
     }
 
     @When("fill username and password")
     public void fill_username_and_password() {
+        SignUp.fillData(getProperty("username"), getProperty("password"));
     }
 
     @Then("the system displays an alert confirmation about the sign up")
     public void the_system_displays_an_alert_confirmation_about_the_sign_up() {
+        Assert.assertTrue(SignUp.isSignUpConfirmationDisplayed());
     }
 }
